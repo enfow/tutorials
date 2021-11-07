d3643d5142d851c1b1662aebb5766ec5a871f7f6
enfow <wgm0601@gmail.com>
2021-11-07 11:56:45 +0900

    Add slack message function

--- finance-with-python/dual_momentum.py
+++ finance-with-python/dual_momentum.py
@@ -12,6 +12,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from typing import Dict
 
+from slack_sender import SlackMsgSender
+
+
 # Type Alias
 Name = str
 FullData = pd.DataFrame
@@ -34,10 +37,20 @@ class DualMomentum:
     def __init__(self, save_dir: str):
         """Initialize."""
         self.today = pd.to_datetime("today")
+        self.today_date = self.today.strftime('%y%m%d')
         self.data: Dict[Name, OpeningPrice] = self.get_opening_price()
         self.save_dir = save_dir
         if not os.path.exists(save_dir):
             os.makedirs(save_dir, exist_ok=True)
+
+    def report(self):
+        """analyze."""
+        yields= {name : self.data[name][-1] for name in self.data}
+        file_path = self.save_plot()
+        msg = f"Momentum 분석: {self.today_date}\n"
+        for name, y in yields.items():
+            msg += f"{name} : {round(y, 3)}%\n"
+        self.send_msg(msg, file_path)
 
     def get_opening_price(self) -> Dict[Name, OpeningPrice]:
         """get data with tikker."""
@@ -63,22 +76,30 @@ class DualMomentum:
         six_month_before = self.today - pd.Timedelta("180 days")
         return series.loc[six_month_before:]
 
-    def save_plot(self):
+    def save_plot(self) -> str:
         """save plot."""
         plt.figure(figsize=(14, 4))
         for name, data in self.data.items():
             plt.plot(data.index, data)
         
-        today_date = self.today.strftime('%y%m%d')
-        plt.title(f"6 months yield at {today_date}")
+        plt.title(f"6 months yield at {self.today_date}")
         plt.legend([f"{name} : {round(self.data[name][-1], 2)}%" for name in TIKKERS])
         plt.grid()
         
-        file_name = f"6M_yield_{today_date}"
-        file_name = os.path.join(self.save_dir, file_name)
-        plt.savefig(file_name)
+        file_name = f"6M_yield_{self.today_date}"
+        file_path = os.path.join(self.save_dir, file_name)
+        plt.savefig(file_path)
 
+        return file_path
+
+    def send_msg(self, text: str, file_path: str) -> None:
+        """send slack msg."""
+        sender =SlackMsgSender(channel_name = "#dual_momentum")
+        if text:
+            sender.send_msg(text=text)
+        if file_path:
+            sender.send_file(file_path=file_path)
 
 if __name__ == "__main__":
     strategy = DualMomentum(save_dir = "img")
-    strategy.save_plot()
+    strategy.report()
