b907b3a49b81b9b7b23d35738d371090fbfc208f
enfow <wgm0601@gmail.com>
2021-11-08 08:45:54 +0900

    Fix docs

--- finance-with-python/dual_momentum.py
+++ finance-with-python/dual_momentum.py
@@ -29,6 +29,7 @@ TIKKERS = {
 }
 START_YEAR = 2021
 DEPOSIT_RATE = 1.008
+TERM = 180
 
 
 class DualMomentum:
@@ -43,14 +44,15 @@ class DualMomentum:
         if not os.path.exists(save_dir):
             os.makedirs(save_dir, exist_ok=True)
 
-    def report(self):
-        """analyze."""
+    def report(self, send_msg=True):
+        """analyze momentum and send slack message."""
         yields= {name : self.data[name][-1] for name in self.data}
         file_path = self.save_plot()
-        msg = f"Momentum 분석: {self.today_date}\n"
-        for name, y in yields.items():
-            msg += f"{name} : {round(y, 3)}%\n"
-        self.send_msg(msg, file_path)
+        if send_msg:
+            msg = f"Momentum 분석: {self.today_date}\n"
+            for name, y in yields.items():
+                msg += f"{name} : {round(y, 3)}%\n"
+            self.send_msg(msg, file_path)
 
     def get_opening_price(self) -> Dict[Name, OpeningPrice]:
         """get data with tikker."""
@@ -73,7 +75,7 @@ class DualMomentum:
 
     def from_six_month_before(self, series: pd.Series):
         """slice dataframe from 6 months ago."""
-        six_month_before = self.today - pd.Timedelta("180 days")
+        six_month_before = self.today - pd.Timedelta(f"{TERM} days")
         return series.loc[six_month_before:]
 
     def save_plot(self) -> str:
@@ -93,7 +95,7 @@ class DualMomentum:
         return file_path
 
     def send_msg(self, text: str, file_path: str) -> None:
-        """send slack msg."""
+        """send slack message with text and file(.txt, .png, etc)."""
         sender =SlackMsgSender(channel_name = "#dual_momentum")
         if text:
             sender.send_msg(text=text)
