768597eb84d8d8ef687fc979d31a7fb9187d3ec3
enfow <wgm0601@gmail.com>
2021-11-07 11:01:11 +0900

    fix method

--- finance-with-python/dual_momentum.py
+++ finance-with-python/dual_momentum.py
@@ -10,15 +10,21 @@ import os
 import FinanceDataReader as fdr
 import matplotlib.pyplot as plt
 import pandas as pd
+from typing import Dict
+
+# Type Alias
+Name = str
+FullData = pd.DataFrame
+OpeningPrice = pd.Series
 
 # Constant
-TIKKER = {
+TIKKERS = {
     "KOSPI" : "069500",
     "SP500" : "143850",
     "NIKKEI" : "241180",
     "EURO" : "195930",
 }
-YEAR = 2021
+START_YEAR = 2021
 DEPOSIT_RATE = 1.008
 
 
@@ -28,47 +34,49 @@ class DualMomentum:
     def __init__(self, save_dir: str):
         """Initialize."""
         self.today = pd.to_datetime("today")
-        self.today_date = self.today.strftime('%y%m%d')
-        self.data = {
-            name : self.preprocessing(fdr.DataReader(TIKKER[name], YEAR)) for name in TIKKER.keys()
-        } 
+        self.data: Dict[Name, OpeningPrice] = self.get_opening_price()
         self.save_dir = save_dir
         if not os.path.exists(save_dir):
             os.makedirs(save_dir, exist_ok=True)
+
+    def get_opening_price(self) -> Dict[Name, OpeningPrice]:
+        """get data with tikker."""
+        data : Dict[Name, OpeningPrice] = {}
+        for name, tikker in TIKKERS.items():
+            series = fdr.DataReader(tikker, START_YEAR)["Open"]
+            series = self.preprocessing(series)
+            data[name] = series
+        return data
+
+    def preprocessing(self, series: pd.Series):
+        """define preprocessing process."""
+        series = self.from_six_month_before(series)
+        series = self.normalize(series)
+        return series
+
+    def normalize(self, series: pd.Series):
+        """normalize with first date data."""
+        return series / series[0]
+
+    def from_six_month_before(self, series: pd.Series):
+        """slice dataframe from 6 months ago."""
+        six_month_before = self.today - pd.Timedelta("180 days")
+        return series.loc[six_month_before:]
 
     def save_plot(self):
         """save plot."""
         plt.figure(figsize=(14, 4))
         for name, data in self.data.items():
             plt.plot(data.index, data)
-      
-        plt.title(f"6 months yield at {self.today_date}")
-        plt.legend([f"{name} : {round(self.data[name][-1], 2)}%" for name in TIKKER.keys()])
+        
+        today_date = self.today.strftime('%y%m%d')
+        plt.title(f"6 months yield at {today_date}")
+        plt.legend([f"{name} : {round(self.data[name][-1], 2)}%" for name in TIKKERS])
         plt.grid()
         
-        file_name = f"6M_yield_{self.today_date}"
+        file_name = f"6M_yield_{today_date}"
         file_name = os.path.join(self.save_dir, file_name)
         plt.savefig(file_name)
-
-    def preprocessing(self, df: pd.DataFrame):
-        """preprocess data."""
-        df = self.get_open_data(df)
-        df = self.from_six_month_before(df)
-        df = self.normalize(df)
-        return df
-
-    def get_open_data(self, df: pd.DataFrame) -> pd.Series:
-        """get open data only"""
-        return df["Open"]
-
-    def normalize(self, data: pd.Series):
-        """normalize with first date data."""
-        return data / data[0]
-
-    def from_six_month_before(self, df: pd.Series):
-        """slice dataframe from 6 months ago."""
-        six_month_before = self.today - pd.Timedelta("180 days")
-        return df.loc[six_month_before:]
 
 
 if __name__ == "__main__":
