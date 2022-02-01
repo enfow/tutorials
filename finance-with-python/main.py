f8508c8267b5005039865142c7622d5615469f8a
enfow <wgm0601@gmail.com>
2022-02-01 13:37:16 +0900

    add executable

---
+++ finance-with-python/main.py
@@ -0,0 +1,19 @@
+"""execute everything in this project.
+
+Author: Kyeongmin Woo
+Email: wgm0601@gmail.com
+"""
+import argparse
+
+from src.strategy.dual_momentum import DualMomentum
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('exec', type=str, default="")
+parser.add_argument('send_msg', type=bool, default=False)
+
+args = parser.parse_args()
+
+if args.exec == "dual_momentum":
+    DualMomentum(save_dir="./img", send_msg=args.send_msg)
+
